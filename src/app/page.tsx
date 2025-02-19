93eee3b567730c4d2d7dd70bb8a3e2cf4bba95bf
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-02-19 21:24:01 +0300

    Import the Hero component and render it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="max-w-[1440px] m-auto xl:px-24 py-10">
+      <Hero />
+    </main>
+  );
 }
