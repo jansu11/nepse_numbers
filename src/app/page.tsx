93049ffc55bed2024bb39032c8d2681448f20167
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-02-25 17:17:54 +0300

    Render the CaseStudies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import CaseStudies from "@/components/CaseStudies";
 import Hero from "@/components/Hero";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <main className="max-w-[1440px] m-auto xl:px-24 py-10">
       <Hero />
+      <Services />
+      <CaseStudies />
     </main>
   );
 }
