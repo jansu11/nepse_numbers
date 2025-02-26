7ea62672509f84bce55c016e34dc41085b6bd637
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-02-26 12:56:18 +0300

    Render Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import CaseStudies from "@/components/CaseStudies";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 
 export default function Home() {
   return (
-    <main className="max-w-[1440px] m-auto xl:px-24 py-10">
+    <main className="max-w-[1440px] m-auto xl:px-24 pt-10">
       <Hero />
       <Services />
       <CaseStudies />
+      <Footer />
     </main>
   );
 }
