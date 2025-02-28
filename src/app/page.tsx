f05267bf03030023f8cc07809b3cdc89f80566c5
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-02-28 11:02:02 +0300

    Render the Contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CaseStudies from "@/components/CaseStudies";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Services />
       <CaseStudies />
+      <Contact />
       <Footer />
     </main>
   );
