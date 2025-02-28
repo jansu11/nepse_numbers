f2b587e060ac6c78968a4d1d739a8eb0f243f392
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-02-28 11:06:14 +0300

    Render the Testimonials component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Services />
       <CaseStudies />
+      <Testimonials />
       <Contact />
       <Footer />
     </main>
