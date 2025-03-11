1d2afb82d803e6f672e57dc997988603cb5fab52
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-11 20:14:30 +0300

    Style and make the footer mobile responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-36 bg-dark rounded-t-[45px] px-[60px] pt-[55px] pb-[50px]">
-      <section className="flex justify-between items-end">
+    <footer className="mt-36 bg-dark lg:rounded-t-[45px] px-[60px] pt-[55px] pb-[50px]">
+      <section className="flex flex-col lg:flex-row gap-12 justify-between items-center lg:items-end">
         <div>
           <Image
             src={"/logo-white.png"}
@@ -13,11 +13,11 @@ const Footer = () => {
             height={29}
           />
         </div>
-        <nav className="flex items-center gap-8 list-none">
+        <nav className="flex flex-col lg:flex-row items-center gap-8 list-none">
           <li className="group block ">
             <a
               href="#!"
-              className="group-hover:text-green transition-all delay-200 flex underline text-gray"
+              className="group-hover:text-green transition-all delay-200 flex hover:underline text-gray"
             >
               About us
             </a>
@@ -25,7 +25,7 @@ const Footer = () => {
           <li className="group block ">
             <a
               href="#!"
-              className="group-hover:text-green transition-all delay-200 flex underline text-gray"
+              className="group-hover:text-green transition-all delay-200 flex hover:underline text-gray"
             >
               Services
             </a>
@@ -33,7 +33,7 @@ const Footer = () => {
           <li className="group block ">
             <a
               href="#!"
-              className="group-hover:text-green transition-all delay-200 flex underline text-gray"
+              className="group-hover:text-green transition-all delay-200 flex hover:underline text-gray"
             >
               Use Cases
             </a>
@@ -41,7 +41,7 @@ const Footer = () => {
           <li className="group block ">
             <a
               href="#!"
-              className="group-hover:text-green transition-all delay-200 flex underline text-gray"
+              className="group-hover:text-green transition-all delay-200 flex hover:underline text-gray"
             >
               Pricing
             </a>
@@ -49,14 +49,15 @@ const Footer = () => {
           <li className="group block ">
             <a
               href="#!"
-              className="group-hover:text-green transition-all delay-200 flex underline text-gray"
+              className="group-hover:text-green transition-all delay-200 flex hover:underline text-gray"
             >
               Blog
             </a>
           </li>
         </nav>
 
-        <div className="flex gap-3">
+        {/* Images to be displayed in large screen devices */}
+        <div className="hidden lg:flex gap-3">
           <span>
             <Image
               src={"/linkedin.svg"}
@@ -78,9 +79,9 @@ const Footer = () => {
           </span>
         </div>
       </section>
-      <section className="flex justify-between mt-[66px]">
-        <div className="flex flex-col justify-between text-white">
-          <h3 className="text-black font-bold inline">
+      <section className="flex flex-col gap-10 lg:gap-3 lg:flex-row justify-between mt-[66px]">
+        <div className="flex flex-col gap-3 lg:gap-0 lg:justify-between text-white text-center lg:text-start">
+          <h3 className="text-black font-bold inline mb-12 lg:mb-0">
             <span className="bg-green px-1 py-0.5 rounded-lg">Contact us:</span>
           </h3>
           <p>Email: info@positivus.com </p>
@@ -90,26 +91,43 @@ const Footer = () => {
             Moonstone City, Stardust State 12345
           </p>
         </div>
-        <div className="bg-black px-10 py-14">
-          <form className="flex gap-5">
+        <div className="bg-[#292A32] px-10 py-14 rounded-[14px]">
+          <form className="flex flex-col lg:flex-row gap-5">
             <input
               type="email"
               placeholder="Email"
-              className="bg-transparent px-9 rounded-[14px] border border-white"
+              className="bg-transparent px-4 xl:px-9 py-5 rounded-[14px] border border-white text-white"
             />
-            <button className="bg-green px-9 py-5 rounded-[14px]">
+            <button className="bg-green px-4 xl:px-9 py-5 rounded-[14px]">
               Subscribe to news
             </button>
           </form>
         </div>
       </section>
+
+      {/* Images to be displayed in small screen devices */}
+      <div className="lg:hidden flex justify-center gap-3 mt-[50px]">
+        <span>
+          <Image src={"/linkedin.svg"} alt="linkedin" width={30} height={30} />
+        </span>
+        <span>
+          <Image src={"/facebook.svg"} alt="linkedin" width={30} height={30} />
+        </span>
+        <span>
+          <Image src={"/twitter.svg"} alt="linkedin" width={30} height={30} />
+        </span>
+      </div>
+
       <hr className="mt-[50px] text-gray" />
-      <div className="mt-[50px] flex gap-8 items-end text-white">
+      <div className="mt-[50px] flex flex-col lg:flex-row items-center lg:items-end gap-8  text-white">
         <p className="text-white">
           &copy; {new Date().getFullYear()} Positivus. All Rights Reserved.
         </p>
 
-        <a href="#!" className="flex underline">
+        <a
+          href="#!"
+          className="flex hover:underline transition-all duration-300"
+        >
           Privacy Policy
         </a>
       </div>
