04e87ba4d65238717fbcbe4caec07ee058d86e36
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-11 20:16:06 +0300

    Make one ckeckbox checked by default

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -16,6 +16,7 @@ const ContactForm = () => {
                   name="option"
                   type="radio"
                   className="peer h-5 w-5 cursor-pointer appearance-none bg-white rounded-full border border-slate-600 checked:border-slate-800 transition-all"
+                  defaultChecked
                   id="hi"
                 />
                 <span className="absolute bg-green w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
