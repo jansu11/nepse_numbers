4be79217b41b83873f449b3b40af379852b9b59d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-12 14:40:32 +0300

    Make footer responsive for small screen devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-36 bg-dark lg:rounded-t-[45px] px-[60px] pt-[55px] pb-[50px]">
+    <footer className="mt-32 lg:mt-36 bg-dark lg:rounded-t-[45px] px-4 lg:px-[60px] pt-[55px] pb-[50px]">
       <section className="flex flex-col lg:flex-row gap-12 justify-between items-center lg:items-end">
         <div>
           <Image
@@ -13,7 +13,7 @@ const Footer = () => {
             height={29}
           />
         </div>
-        <nav className="flex flex-col lg:flex-row items-center gap-8 list-none">
+        <nav className="flex flex-col lg:flex-row items-center gap-4 lg:gap-8 list-none">
           <li className="group block ">
             <a
               href="#!"
@@ -81,7 +81,7 @@ const Footer = () => {
       </section>
       <section className="flex flex-col gap-10 lg:gap-3 lg:flex-row justify-between mt-[66px]">
         <div className="flex flex-col gap-3 lg:gap-0 lg:justify-between text-white text-center lg:text-start">
-          <h3 className="text-black font-bold inline mb-12 lg:mb-0">
+          <h3 className="text-black font-bold inline mb-10 lg:mb-0">
             <span className="bg-green px-1 py-0.5 rounded-lg">Contact us:</span>
           </h3>
           <p>Email: info@positivus.com </p>
@@ -92,13 +92,13 @@ const Footer = () => {
           </p>
         </div>
         <div className="bg-[#292A32] px-10 py-14 rounded-[14px]">
-          <form className="flex flex-col lg:flex-row gap-5">
+          <form className="flex flex-col md:flex-row gap-5">
             <input
               type="email"
               placeholder="Email"
-              className="bg-transparent px-4 xl:px-9 py-5 rounded-[14px] border border-white text-white"
+              className="bg-transparent px-4 xl:px-9 py-3 md:py-5 rounded-[14px] border border-white text-white w-full"
             />
-            <button className="bg-green px-4 xl:px-9 py-5 rounded-[14px]">
+            <button className="bg-green px-4 xl:px-9 py-3 md:py-5 rounded-[14px] w-full">
               Subscribe to news
             </button>
           </form>
@@ -106,7 +106,7 @@ const Footer = () => {
       </section>
 
       {/* Images to be displayed in small screen devices */}
-      <div className="lg:hidden flex justify-center gap-3 mt-[50px]">
+      <div className="lg:hidden flex justify-center gap-3 mt-10 lg:mt-[50px]">
         <span>
           <Image src={"/linkedin.svg"} alt="linkedin" width={30} height={30} />
         </span>
@@ -118,8 +118,8 @@ const Footer = () => {
         </span>
       </div>
 
-      <hr className="mt-[50px] text-gray" />
-      <div className="mt-[50px] flex flex-col lg:flex-row items-center lg:items-end gap-8  text-white">
+      <hr className="mt-10 lg:mt-[50px] text-gray" />
+      <div className="mt-10 lg:mt-[50px] flex flex-col lg:flex-row items-center lg:items-end gap-4 lg:gap-8  text-white">
         <p className="text-white">
           &copy; {new Date().getFullYear()} Positivus. All Rights Reserved.
         </p>
