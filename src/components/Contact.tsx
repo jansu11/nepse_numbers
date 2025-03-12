c88c66d26b012701aa842445711903591629e542
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-12 14:43:29 +0300

    Make component responsive for small screen devices

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,8 +3,8 @@ import ContactForm from "./ContactForm";
 
 const Contact = () => {
   return (
-    <section className="pt-36">
-      <div className="flex items-center gap-10">
+    <section className="pt-36 px-4">
+      <div className="flex flex-col items-center text-center md:text-start md:flex-row gap-5 md:gap-10">
         <h2 className="bg-green text-3xl font-bold p-2 rounded-md">
           Contact Us
         </h2>
