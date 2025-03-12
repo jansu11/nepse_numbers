d38e37b57d190f3fddfa2bb9d90aa26de3a341c2
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-12 17:32:17 +0300

    Make the Team component mobile responsive

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -3,22 +3,22 @@ import TeamCard, { TeamCardProps } from "./TeamCard";
 
 const Team = () => {
   return (
-    <section className="pt-36">
-      <div className="flex items-center gap-10">
+    <section className="pt-24 md:pt-36 px-4">
+      <div className="flex flex-col md:flex-row text-center md:text-start items-center gap-5 md:gap-10">
         <h2 className="bg-green text-3xl font-bold p-2 rounded-md">Team</h2>
         <p className="max-w-md">
           Meet the skilled and experienced team behind our successful digital
           marketing strategies
         </p>
       </div>
-      <div className="grid grid-cols-3 gap-10 pt-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 pt-8 md:pt-16">
         {TeamDetails.map((det, idx) => (
           <TeamCard key={idx} {...det} />
         ))}
       </div>
-      <div className="justify-self-end mt-10">
+      <div className="sm:justify-self-end mt-10">
         <a href="#!" className="block">
-          <button className="bg-dark text-white px-9 py-5 rounded-[14px]">
+          <button className="w-full bg-dark text-white px-9 py-5 rounded-[14px]">
             See all team
           </button>
         </a>
