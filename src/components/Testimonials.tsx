64f8a57114937d64722f7f2be13609a034f6155f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-12 15:24:50 +0300

    Make testimonials header responsive

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,7 +4,7 @@ import TestimonialsCard from "./TestimonialsCard";
 const Testimonials = () => {
   return (
     <section className="pt-28">
-      <div className="flex items-center gap-10">
+      <div className="flex flex-col md:flex-row text-center md:text-start items-center gap-10 px-4">
         <h2 className="bg-green text-3xl font-bold p-2 rounded-md">
           Testimonials
         </h2>
