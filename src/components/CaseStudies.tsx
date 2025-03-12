48779e610f379ed838b30c29d6a24fb6a7d6cb22
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-12 18:32:41 +0300

    Make component mobile responsive

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const CaseStudies = () => {
   return (
-    <section className="pt-36">
-      <div className="flex items-center gap-10">
+    <section className="pt-24 md:pt-36 px-4">
+      <div className="flex flex-col sm:flex-row text-center sm:text-start items-center gap-5 sm:gap-10">
         <h2 className="bg-green text-3xl font-bold p-2 rounded-md">
           Case Studies
         </h2>
@@ -13,8 +13,8 @@ const CaseStudies = () => {
           through Our Case Studies
         </p>
       </div>
-      <div className="mt-16 bg-dark py-[70px] px-[60px] rounded-[45px] flex gap-16">
-        <article className="flex flex-col items-start gap-5 border-r-2 pr-16 border-gray">
+      <div className="mt-8 lg:mt-16 lg:bg-dark lg:py-[70px] lg:px-[60px] lg:rounded-[45px] flex flex-wrap lg:flex-nowrap gap-8 lg:gap-16">
+        <article className="bg-dark p-10 rounded-[45px] lg:bg-none lg:p-0 lg:rounded-none flex flex-col items-start gap-5 border-r-2 lg:pr-16 border-gray">
           <p className="text-gray text-sm">
             For a local restaurant, we implemented a targeted PPC campaign that
             resulted in a 50% increase in website traffic and a 25% increase in
@@ -31,7 +31,7 @@ const CaseStudies = () => {
             />{" "}
           </button>
         </article>
-        <article className="flex flex-col items-start gap-5 pr-16 border-r-2 border-gray">
+        <article className="bg-dark p-10 rounded-[45px] lg:bg-none lg:p-0 lg:rounded-none flex flex-col items-start gap-5 lg:pr-16 border-r-2 border-gray">
           <p className="text-gray text-sm">
             For a B2B software company, we developed an SEO strategy that
             resulted in a first page ranking for key keywords and a 200%
@@ -48,7 +48,7 @@ const CaseStudies = () => {
             />{" "}
           </button>
         </article>
-        <article className="flex flex-col items-start gap-5">
+        <article className="bg-dark p-10 rounded-[45px] lg:bg-none lg:p-0 lg:rounded-none flex flex-col items-start gap-5">
           <p className="text-gray text-sm">
             For a national retail chain, we created a social media marketing
             campaign that increased followers by 25% and generated a 20%
