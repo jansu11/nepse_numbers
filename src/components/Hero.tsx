7ebc3f2ea2ac49c5ab4e9b7669ed96703724c7b2
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2025-03-13 10:57:28 +0300

    Style the Hero section and make it mobile responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,23 +4,31 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>
+    <div className="px-4">
       <Header />
-      <section className="flex justify-between py-16 just">
-        <article>
-          <h1 className="font-medium text-6xl/normal max-w-xl l">
+      <section className="flex justify-between py-12 pb-10 lg:py-16 just">
+        <article className="flex flex-col items-center">
+          <h1 className="font-medium text-4xl/normal md:text-6xl/normal lg:max-w-xl">
             Navigating the digital landscape for success
           </h1>
-          <p className="text-xl py-9 max-w-lg">
+          <div className="lg:hidden mt-9 lg:mt-0">
+            <Image
+              src={"/speaker.png"}
+              alt="speaker image"
+              width={560}
+              height={450}
+            />
+          </div>
+          <p className="text-lg md:text-xl py-9 lg:max-w-lg">
             Our digital marketing agency helps businesses grow and succeed
             online through a range of services including SEO, PPC, social media
             marketing, and content creation.
           </p>
-          <button className="px-9 py-4 bg-black text-gray rounded-xl">
+          <button className="w-full lg:w-auto lg:self-start px-9 py-4 bg-black text-gray rounded-xl">
             Book a consultation
           </button>
         </article>
-        <div>
+        <div className="hidden lg:block">
           <Image
             src={"/speaker.png"}
             alt="speaker image"
@@ -29,7 +37,7 @@ const Hero = () => {
           />
         </div>
       </section>
-      <section className="flex justify-between">
+      <section className="flex justify-center lg:justify-start flex-wrap gap-x-8 gap-y-3">
         <div>
           <Image
             className="grayscale"
