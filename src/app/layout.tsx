ad29cd4aa363e417a50ea93e0a939710dd23678d
Sujan Bohora <sujan.boro22@gmail.com>
2025-07-07 22:27:14 +0545

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,7 @@ const spaceGrotesk = Space_Grotesk({
 
 export const metadata: Metadata = {
   title: "Nepse Numbers",
-  description:
-    "The Positivus is a beautiful and functional landing page design created specifically for digital marketing agencies. With its clean and modern design, Positivus is the perfect template to showcase your agency's services and case studies to potential clients",
+  description:" Your one stop for data and analysis regarding NEPSE.",
 };
 
 export default function RootLayout({
